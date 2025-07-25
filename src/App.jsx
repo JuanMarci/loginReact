b1b8b3c1781b764183998590e9c28d5de0a67760
Juanca <juan_cmarcillo@soy.sena.edu.co>
2025-07-24 20:48:28 -0500

    Inicio Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -39,9 +39,10 @@ function App() {
       setlogueado(true)
     }
   }
-  useEffect(() =>{
+
+  useEffect(() => {
     validar()
-  }, [])  
+  }, [])
 
   if (logueado) {
     return <Conversor/>  
